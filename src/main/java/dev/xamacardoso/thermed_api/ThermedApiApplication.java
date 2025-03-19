c12a9f632f18ad25cca183d93dd2ba9ca09eebde
Xamacardoso <xcardoso1997@gmail.com>
2025-03-19 18:40:57 -0300

    Initial commit

---
+++ src/main/java/dev/xamacardoso/thermed_api/ThermedApiApplication.java
@@ -0,0 +1,13 @@
+package dev.xamacardoso.thermed_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/xamacardoso/thermed_api/ThermedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xamacardoso.thermed_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
